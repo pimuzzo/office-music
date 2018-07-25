03af69acba4eb30b59fb31b33e2d1386e38c4e8f
Simone Locci <simone.locci@cloudesire.com>
2018-07-25 12:55:28 +0200

    Fix background script

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,14 +1,13 @@
 chrome.runtime.onMessage.addListener(function(request, sender, callback) {
-  if (request.webdisUrl) {
+  if(request.webdisUrl) {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', request.webdisUrl, true);
     xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4) {
-        // JSON.parse does not evaluate the attacker's scripts.
-        // var resp = JSON.parse(xhr.responseText);
+      if(this.readyState == 4) {
+        if(this.status == 200) alert('request sent');
+        if(this.status != 200) alert('something went wrong');
       }
     };
     xhr.send();
-    return xhr.responseText;
   }
 });
