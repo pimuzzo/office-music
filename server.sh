d7616233d198051c6d4ab0678d65507e36fc1451
Simone Locci <simone.locci@cloudesire.com>
2018-10-25 10:10:59 +0200

    Fix music quality

--- server.sh
+++ server.sh
@@ -8,7 +8,7 @@ while true; do
   CURRENT_SONG=$(redis-cli -h "$REDIS_HOST" RPOP current_song)
   if [ "$CURRENT_SONG" != "" ]; then
     youtube-dl --extract-audio --audio-format best --output "current_song.%(ext)s" "$CURRENT_SONG"
-    omxplayer -o both current_song.*
+    omxplayer -o local current_song.*
     rm current_song.*
   else
     sleep 1s
