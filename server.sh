2e2e286de4fd342ad7531ffed5692d3121f4e9b8
Simone Locci <simone.locci@cloudesire.com>
2018-11-24 12:43:29 +0100

    Add env and env.local files

--- server.sh
+++ server.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Start config
-REDIS_HOST=localhost
+# Start config - define your customizations inside .env.local
+source .env
 # End config
 
 while true; do 
   CURRENT_SONG=$(redis-cli -h "$REDIS_HOST" RPOP current_song)
   if [ "$CURRENT_SONG" != "" ]; then
     youtube-dl --extract-audio --audio-format best --output "current_song.%(ext)s" "$CURRENT_SONG"
-    omxplayer -o local current_song.*
+    omxplayer -o "$OMX_AUDIO_OUT_DEVICE" current_song.*
     rm current_song.*
   else
     sleep 1s
