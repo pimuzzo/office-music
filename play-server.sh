da2b934db9beef071a7bb2d83f0a457749c9c2b7
Simone Locci <simonelocci88@gmail.com>
2019-10-17 09:34:47 +0200

    Add playlist support (#11)

---
+++ play-server.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Start config - define your customizations inside .env.local
+source "$(dirname "$0")/.env"
+# End config
+
+rm -f songs/*
+
+while true; do
+  echo "Searching for songs to play..."
+  for file in songs/* ; do
+    if [[ $file != "songs/*" && ${file: -5} != ".part" ]]; then
+      filename=${file}
+      echo "Playing $filename..."
+      omxplayer -o "$SERVER_OMX_AUDIO_OUT_DEVICE" "$filename"
+      rm -f "$filename"
+    fi
+  done
+  sleep 1s
+done
+
